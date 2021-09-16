02de6a395b82c93f10322d7e99f410a164e2c009
方和新 <fanghexin@bonree.com>
2021-09-16 16:11:16 +0800

    self gateway

---
+++ src/main/java/self/practice/codes/io/week02/gateway/HttpServer02.java
@@ -0,0 +1,52 @@
+package self.practice.codes.io.week02.gateway;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * 为每个请求起一个线程 Socket 版本
+ * @author Crayon
+ */
+public class HttpServer02 {
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket socket = new ServerSocket(8081);
+            while (true) {
+                Socket accept = socket.accept();
+                new Thread(() -> response(accept)).start();
+            }
+        } catch (IOException e) {
+            System.err.println("response(): " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * https://stackoverflow.com/questions/9260126/what-are-the-differences-between-char-literals-n-and-r-in-java
+     * https://stackoverflow.com/questions/10788125/a-simple-http-server-with-java-socket
+     */
+    private static void response(Socket socket) {
+        try (OutputStream outputStream = socket.getOutputStream();
+             OutputStreamWriter optWriter = new OutputStreamWriter(outputStream))
+        {
+            String body = "hello,nio1";
+
+            optWriter.write("HTTP/1.1 200 OK\r\n");
+            optWriter.write("Content-Type:text/html;charset=utf-8\r\n");
+            optWriter.write("Content-Length:" + body.getBytes().length + "\r\n");
+            optWriter.write("\r\n");
+            optWriter.write(body);
+            optWriter.flush();
+            socket.close();
+        } catch (IOException e) {
+            System.err.println("response(): " + e.getMessage());
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ src/main/java/self/practice/codes/io/week02/gateway/HttpServer03.java
@@ -0,0 +1,56 @@
+package self.practice.codes.io.week02.gateway;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 请求提交到线程池处理 Socket 版本
+ * @author Crayon
+ */
+public class HttpServer03 {
+
+    public static void main(String[] args) {
+        try {
+            ExecutorService executorService = Executors.newFixedThreadPool(
+                    Runtime.getRuntime().availableProcessors() * 4);
+            ServerSocket socket = new ServerSocket(8081);
+            while (true) {
+                Socket accept = socket.accept();
+                executorService.execute(() -> response(accept));
+            }
+        } catch (IOException e) {
+            System.err.println("response(): " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * https://stackoverflow.com/questions/9260126/what-are-the-differences-between-char-literals-n-and-r-in-java
+     * https://stackoverflow.com/questions/10788125/a-simple-http-server-with-java-socket
+     */
+    private static void response(Socket socket) {
+        try (OutputStream outputStream = socket.getOutputStream();
+             OutputStreamWriter optWriter = new OutputStreamWriter(outputStream))
+        {
+            String body = "hello,nio1";
+
+            optWriter.write("HTTP/1.1 200 OK\r\n");
+            optWriter.write("Content-Type:text/html;charset=utf-8\r\n");
+            optWriter.write("Content-Length:" + body.getBytes().length + "\r\n");
+            optWriter.write("\r\n");
+            optWriter.write(body);
+            optWriter.flush();
+            socket.close();
+        } catch (IOException e) {
+            System.err.println("response(): " + e.getMessage());
+            e.printStackTrace();
+        }
+
+    }
+}
